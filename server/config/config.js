a2ebec83b7152526f5d27b0cfc337844b8751df4
javier <javier_herrera_hidalgo@carrefour.com>
2019-07-25 13:07:01 +0200

    REST-SERVER-USUARIO

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,37 @@
 //===========================
 
 process.env.PORT = process.env.PORT  || 3000;
+
+
+
+//=============================
+// entorno
+//===========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+
+
+//=============================
+// entorno
+//===========================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+
+    urlDB = 'mongodb+srv://LordHerrera:tMNOihn1KJ7akrec@cluster0-jfirq.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
+
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 
 require('./config/config.js')
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
 
 
 const bodyParser = require('body-parser');
@@ -13,41 +15,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
-});
 
-app.post('/usuario/', function (req, res) {
 
-    let body = req.body;
 
-    if (body.nombre === undefined) {
 
-        res.status(400).json({
-            ok:false,
-            mensaje: 'el nombre es necesario'
-        });
-    } else {
 
-        res.json({
-            persona: body
-        });
-    }
-});
+mongoose.connect(process.env.URLDB,
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-app.put('/usuario/:id', function (req, res) {
+        if (err) throw new err;
 
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
+        console.log('Base de datos ONLINE');
+    });
 
 
 app.listen(process.env.PORT, () => {
